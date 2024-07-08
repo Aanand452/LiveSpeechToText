818be3030deb2a8d86d192a26b17d911e114997a
apple <apple@apples-MacBook-Pro.local>
2024-07-08 23:33:55 +0530

    livespeechtotext

---
+++ SpeechToText.py
@@ -0,0 +1,51 @@
+# Python3 program to trasnslate
+#speech to text
+
+import speech_recognition as sr
+# import pyttsx3
+
+
+
+# initialize the recognizer
+
+r = sr.Recognizer()
+
+
+def record_text():
+    #loop incase of errors
+    while(1):
+        try:
+            # use the microphone as source for input.
+            with sr.Microphone() as source2:
+                #Prepare recognizer to receive input
+                r.adjust_for_ambient_noise(source2,duration=0.2)
+                #listens for the user's input
+                audio2 = r.listen(source2)
+                # Using google for voice recognition
+                
+                MyText = r.recognize_google(audio2)
+                MyText = MyText.lower()
+                print(f"Did you say {MyText}")
+                
+                return MyText
+        
+        except sr.RequestError as e:
+            print("Could not request results;{0}".format(e))
+            
+        except sr.UnknownValueError:
+            print("Unknown error occurred")
+        
+    return
+
+def output_text(text):
+    f = open("output.txt","a")
+    f.write(text)
+    f.write("\n")
+    f.close()
+    return
+
+while(1):
+    text = record_text()
+    output_text(text)
+    
+    print("Wrote to text")
